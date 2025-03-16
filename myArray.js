0a2885c199fedc2e83520de09b89485efdba2285
saijamii <saijami38@gmail.com>
2025-03-16 11:58:26 +0530

    Arrays , Tuples

---
+++ myArray.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superHeros = [];
+// const heroPower: number[] = [];
+var heroPower = [];
+var allUsers = [];
+var MLMOdels = [
+    [255, 265, 255],
+    // ["sdg"]
+];
+superHeros.push("spiderman");
+heroPower.push(2);
+allUsers.push({
+    name: "",
+    isActive: false,
+});
---
+++ myArray.ts
@@ -0,0 +1,25 @@
+const superHeros: string[] = [];
+// const heroPower: number[] = [];
+const heroPower: Array<number> = [];
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: Array<User> = [];
+
+const MLMOdels: Array<number>[] = [
+  [255, 265, 255],
+  // ["sdg"]
+];
+
+superHeros.push("spiderman");
+heroPower.push(2);
+
+allUsers.push({
+  name: "",
+  isActive: false,
+});
+
+export {};
