c6c11418e4e59d894170e39b68471a7fb48f02b4
saijamii <saijami38@gmail.com>
2025-03-16 11:32:02 +0530

    variable changes

--- variables.ts
+++ variables.ts
@@ -1,5 +1,7 @@
-let greetings = "Hello saijami";
-
+let greetings: string = "Hello saijami";
+let myNum: number = 6;
+greetings.toLowerCase();
 console.log(greetings);
+console.log(myNum);
 
 export {};
