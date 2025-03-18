7e645e740a92e06fdb4fedcaf3b6f8d49ae84033
saijamii <saijami38@gmail.com>
2025-03-18 21:58:22 +0530

    js files removed

--- myTuples.js
+++
@@ -1,9 +0,0 @@
-// const user: (string | number)[] = [1,"sj"];
-var user = ["sj", 22, true];
-var rgb = [255, 265, 221];
-var newUser = [112, "sj@gmail.com"];
-newUser[1] = "gt.com";
-// newUser[2] = 52
-newUser.push("2");
-newUser.push("5");
-newUser.push(5);
--- variables.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var greetings = "Hello saijami";
-console.log(greetings);
