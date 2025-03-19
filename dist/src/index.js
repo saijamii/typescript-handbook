330a7e0d017b6239e40d0458f335e30db21b26c2
saijami <saijami@unosimple.com>
2025-03-19 18:49:36 +0530

    public private

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,10 +1,20 @@
 "use strict";
+// class User {
+//   public email: string;
+//   private name: string;
+//   readonly city: string = "";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
 class User {
-    constructor(email, name) {
-        this.city = "";
+    constructor(email, name, userId) {
         this.email = email;
         this.name = name;
+        this.userId = userId;
+        this.city = "";
     }
 }
 const saijami = new User("s@gmail.com", "sj");
-// saijami.city = "vizag";
+// saijami.name;
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
+// class User {
+//   public email: string;
+//   private name: string;
+//   readonly city: string = "";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
 class User {
-  email: string;
-  name: string;
   readonly city: string = "";
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
-  }
+  constructor(
+    public email: string,
+    public name: string,
+    private userId?: string
+  ) {}
 }
 
 const saijami = new User("s@gmail.com", "sj");
-// saijami.city = "vizag";
+// saijami.name;
