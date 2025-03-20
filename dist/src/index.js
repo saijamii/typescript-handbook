179e114475952718bfc377301fe52e53b483efb5
saijami <saijami@unosimple.com>
2025-03-20 18:38:18 +0530

    getters / Setters

--- dist/src/index.js
+++ dist/src/index.js
@@ -13,8 +13,25 @@ class User {
         this.email = email;
         this.name = name;
         this.userId = userId;
+        this._courseCount = 1;
         this.city = "";
+    }
+    deleteToken() {
+        console.log("token deleted");
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get getCourseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count shold be more than 1");
+        }
+        this._courseCount = courseNum;
     }
 }
 const saijami = new User("s@gmail.com", "sj");
 // saijami.name;
+// saijami.deleteToken()
--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,34 @@
 // }
 
 class User {
+  private _courseCount = 1;
   readonly city: string = "";
   constructor(
     public email: string,
     public name: string,
     private userId?: string
   ) {}
+
+  private deleteToken() {
+    console.log("token deleted");
+  }
+
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+
+  get getCourseCount(): number {
+    return this._courseCount;
+  }
+
+  set courseCount(courseNum: number) {
+    if (courseNum <= 1) {
+      throw new Error("Course count shold be more than 1");
+    }
+    this._courseCount = courseNum;
+  }
 }
 
 const saijami = new User("s@gmail.com", "sj");
 // saijami.name;
+// saijami.deleteToken()
