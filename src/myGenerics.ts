a2165bad5bf5e0cd5c2f0931c96bc5d53815a386
saijamii <saijami38@gmail.com>
2025-03-24 21:53:27 +0530

    In Operator

--- src/myGenerics.ts
+++ src/myGenerics.ts
@@ -79,3 +79,24 @@ class sellable<T> {
     this.cart.push(product);
   }
 }
+
+interface User {
+  name: string;
+  email: string;
+}
+
+interface Admin {
+  name: string;
+  email: string;
+  isAdmin: boolean;
+}
+
+function isAdminAccount(account: User | Admin) {
+  //  return account.isAdmin
+
+  if ("isAdmin" in account) {
+    return account.isAdmin;
+  }
+
+  return false;
+}
