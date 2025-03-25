9ecdfc7de0b262b4d7298fc72beb2336f38d75f1
saijami <saijami@unosimple.com>
2025-03-25 18:37:29 +0530

    instanceof / predicates

--- src/detection.ts
+++ src/detection.ts
@@ -26,3 +26,35 @@ function printAll(strs: string | string[] | null) {
     }
   }
 }
+
+// instanceof narrowing
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
+}
+
+// type predicates
+type Fish = {
+  swim: () => void;
+};
+
+type Bird = {
+  fly: () => void;
+};
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet;
+    return "Fish Food";
+  } else {
+    pet;
+    return "Bird Food";
+  }
+}
