b28fc01df20beebe27e1bfd945b102b25ee1a0cf
saijami <saijami@unosimple.com>
2025-03-25 18:59:10 +0530

    never check

--- src/detection.ts
+++ src/detection.ts
@@ -58,3 +58,42 @@ function getFood(pet: Fish | Bird) {
     return "Bird Food";
   }
 }
+
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+interface Square {
+  kind: "square";
+  side: number;
+}
+
+interface Rectangle {
+  kind: "rectangle";
+  length: number;
+  width: number;
+}
+
+type Shape = Circle | Square | Rectangle;
+
+// Never Type
+function getShape(shape: Shape) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  }
+  // return shape.side * shape.side;
+}
+
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+    case "square":
+      return shape.side * shape.side;
+    case "rectangle":
+      return shape.length * shape.width;
+    default:
+      const _defaultShape: never = shape;
+      return _defaultShape;
+  }
+}
